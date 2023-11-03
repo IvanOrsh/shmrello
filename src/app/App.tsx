e8a9ab0cd770d24618ff62ef09c0135ecafe6643
IvanOrsh <ksironov@mail.ru>
2023-11-03 23:26:11 +0300

    chore: initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <h1
+      style={{
+        fontSize: "var(--fs-primary-heading)",
+      }}
+    >
+      Getting Started
+    </h1>
+  );
+}
+
+export default App;
