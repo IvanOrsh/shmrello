f052adfa102874132f9c902f827b871142c6a1da
IvanOrsh <ksironov@mail.ru>
2023-11-04 11:15:25 +0300

    chore: scaffold dark theme

---
+++ src/app/styles/themes/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme, Theme } from "@mui/material";
+
+const theme: Theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      // default: "#BEA4FF"
+      default: "#1D1F26",
+    },
+  },
+});
+
+export default theme;
