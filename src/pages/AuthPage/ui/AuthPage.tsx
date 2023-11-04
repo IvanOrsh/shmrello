ecfa9922dd2cd7e1f7a45d98271c22ff47137556
IvanOrsh <ksironov@mail.ru>
2023-11-04 13:41:26 +0300

    chore: scaffold login form

--- src/pages/AuthPage/ui/AuthPage.tsx
+++ src/pages/AuthPage/ui/AuthPage.tsx
@@ -1,10 +1,4 @@
-import {
-  Container,
-  Stack,
-  OutlinedInput,
-  Button,
-  Typography,
-} from "@mui/material";
+import { Container, Stack, TextField, Button, Typography } from "@mui/material";
 
 import ImageEl from "../../../shared/ui/ImageEl/ImageEl";
 
@@ -12,14 +6,45 @@ import logoImg from "../../../shared/assets/shmrello-logo.svg";
 
 const AuthPage = () => {
   return (
-    <Container>
-      <Stack>
+    <Container
+      maxWidth="xs"
+      sx={{
+        mt: 10 /* x8px */,
+      }}
+    >
+      <Stack mb={6} spacing={4} alignItems="center" textAlign="center">
         <div>
           <ImageEl src={logoImg} alt="logo" />
         </div>
-        <Typography variant="h1" component="h2">
-          AuthPage
-        </Typography>
+        <Stack>
+          <Typography
+            // color="rgba(255, 255, 255, .6)"
+            sx={(theme) => ({
+              color: `${theme.palette.primary.main}`,
+            })}
+            variant="body2"
+            component="p"
+          >
+            Increase your productivity by organizing your workflow in a simple
+            way.
+          </Typography>
+          <Typography
+            sx={(theme) => ({
+              color: `${theme.palette.secondary.main}`,
+            })}
+            variant="body2"
+            component="p"
+          >
+            Keep track of your tasks.
+          </Typography>
+        </Stack>
+      </Stack>
+
+      <Stack spacing={2}>
+        <TextField label="Email" />
+        <TextField label="Password" />
+        <Button variant="outlined">Login</Button>
+        <Typography textAlign="center">Do not have an account?</Typography>
       </Stack>
     </Container>
   );
