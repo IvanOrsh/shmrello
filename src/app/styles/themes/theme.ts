c39c17812f8728fa88dd36cfeee7a717c35dfda3
IvanOrsh <ksironov@mail.ru>
2023-11-04 13:41:00 +0300

    chore: update (dark) theme

--- src/app/styles/themes/theme.ts
+++ src/app/styles/themes/theme.ts
@@ -3,10 +3,20 @@ import { createTheme, Theme } from "@mui/material";
 const theme: Theme = createTheme({
   palette: {
     mode: "dark",
+    primary: {
+      main: "#9fc7e3",
+    },
+    secondary: {
+      main: "#e3e8ae",
+    },
     background: {
       // default: "#BEA4FF"
       default: "#1D1F26",
     },
+  },
+
+  typography: {
+    fontFamily: "Lato, sans-serif",
   },
 });
 
