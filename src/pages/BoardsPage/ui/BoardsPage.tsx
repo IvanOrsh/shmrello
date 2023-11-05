f478ec39f34accf80b0a78312d0f37905ae60de3
IvanOrsh <ksironov@mail.ru>
2023-11-05 13:58:08 +0300

    chore: update boards page

--- src/pages/BoardsPage/ui/BoardsPage.tsx
+++ src/pages/BoardsPage/ui/BoardsPage.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+import { Stack, Typography } from "@mui/material";
+
 import { CreateBoardModal } from "@features/createBoard";
 import { TopBar } from "@widgets/TopBar";
 
 const BoardsPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
-      <TopBar />
+      <TopBar openModal={() => setShowModal(true)} />
 
-      <CreateBoardModal />
+      <CreateBoardModal open={showModal} setOpen={setShowModal} />
 
-      <div>Boards Page</div>
+      <Stack mt={15} spacing={1} alignItems="center">
+        <Typography
+          sx={(theme) => ({
+            color: theme.palette.secondary.main,
+          })}
+          variant="h5"
+        >
+          No boards created.
+        </Typography>
+        <Typography>Create your first board today!</Typography>
+      </Stack>
     </>
   );
 };
