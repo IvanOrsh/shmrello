5aac78f6ce2524513e690e4849cedd0b8ff51d7a
IvanOrsh <ksironov@mail.ru>
2023-11-06 16:20:19 +0300

    feat: task ui

---
+++ src/entities/Task/ui/Task/Task.tsx
@@ -0,0 +1,33 @@
+import { Stack, Typography, IconButton, Icon } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+type TaskProps = {
+  id: string;
+  text: string;
+};
+
+const Task = ({ id, text }: TaskProps) => {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      spacing={1}
+      justifyContent="space-between"
+    >
+      <Typography
+        p={1}
+        width="100%"
+        border="1px solid"
+        borderColor="#777980"
+        bgcolor="#45474e"
+      >
+        {text}
+      </Typography>
+      <IconButton>
+        <DeleteIcon />
+      </IconButton>
+    </Stack>
+  );
+};
+
+export default Task;
