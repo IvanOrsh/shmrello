8bb1941e3751e0b6cbe96e384cd6482b0350f005
IvanOrsh <ksironov@mail.ru>
2023-11-07 03:15:55 +0300

    fix: small screen friendly

--- src/widgets/TopBar/ui/TopBar.tsx
+++ src/widgets/TopBar/ui/TopBar.tsx
@@ -1,9 +1,18 @@
-import { AppBar, Toolbar, Button, Stack } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  IconButton,
+  Stack,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { signOut } from "firebase/auth";
 
 import ImageEl from "@shared/ui/ImageEl/ImageEl";
 import logoImg from "@shared/assets/shmrello-logo-dark.svg";
 import LogoutIcon from "@mui/icons-material/ExitToApp";
+import CreateBoardIcon from "@mui/icons-material/AddCircle";
 import { auth } from "@app/firebase";
 
 type TopBarProps = {
@@ -11,6 +20,9 @@ type TopBarProps = {
 };
 
 const TopBar = ({ openModal }: TopBarProps) => {
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.only("xs"));
+
   const logoutHandler = () => {
     signOut(auth);
   };
@@ -30,16 +42,29 @@ const TopBar = ({ openModal }: TopBarProps) => {
         />
 
         <Stack direction="row" spacing={2}>
-          <Button onClick={openModal} variant="contained">
-            Create Board
-          </Button>
-          <Button
-            onClick={logoutHandler}
-            startIcon={<LogoutIcon />}
-            color="inherit"
-          >
-            Logout
-          </Button>
+          {isXs ? (
+            <>
+              <IconButton color="secondary" onClick={openModal}>
+                <CreateBoardIcon />
+              </IconButton>
+              <IconButton onClick={logoutHandler}>
+                <LogoutIcon />
+              </IconButton>
+            </>
+          ) : (
+            <>
+              <Button onClick={openModal} variant="contained">
+                Create Board
+              </Button>
+              <Button
+                onClick={logoutHandler}
+                startIcon={<LogoutIcon />}
+                color="inherit"
+              >
+                Logout
+              </Button>
+            </>
+          )}
         </Stack>
       </Toolbar>
     </AppBar>
